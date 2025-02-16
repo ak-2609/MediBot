27f6966acdebaefce0c2d252bc567748a3c91f57
Anushka Kumar <anushkaskumar2004@gmail.com>
2025-02-16 12:44:09 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Anushka Kumar',
+    author_email= 'anushkaskumar2004@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
